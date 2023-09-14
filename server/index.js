782448e0a8c004b729afd264be758c0fa1d8fc53
Muhammad-Noman123 <hnoman3744@gmail.com>
2023-09-14 15:43:02 +0500

    asd

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const EmployeeModel = require('./models/Employee')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}))
+app.use(cookieParser())
+
+
+
+mongoose.connect("mongodb+srv://hmdnoman99:hnoman99@hnoman.uvs02iv.mongodb.net/first");
+
+
+app.post('/login', (req, res) =>{
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email:email})
+    .then(user => {
+        if(user) {
+            bcrypt.compare(password, user.password, (err, response) =>{
+                if(response) {
+                    const token = jwt.sign({email: user.email}, "jwt-secret-key", {expiresIn: "1d"})
+                    res.cookie("token", token);
+                    res.json("Success")
+                } else{
+                    res.json("the password is incorrect")
+                }
+            })
+        }else {
+            res.json("No record existed")
+        }
+    })
+})
+
+app.post('/register' , (req, res) =>{
+    const {name, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash =>{
+        EmployeeModel.create({name, email, password: hash})
+        .then(second => res.json(second))
+        .catch(err => res.json(err))
+    }) .catch(err => console.log(err.message))
+
+    })
+   
+
+
+
+
+app.listen(5000, () =>{
+    console.log("server is running")
+})
